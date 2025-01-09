2287792ee3d0db237ddb74e9d836e94f2bfa9b1e
Hiroshi Sano <hrs.sano645@gmail.com>
2025-01-10 08:46:48 +0900

    fix title

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ last_run_time_path = Path("./last_run_time.txt")
 
 st.set_page_config(
     layout="wide",
-    page_title="[beta]Startup Weekend Map for Japan",
+    page_title="Startup Weekend Map for Japan",
 )
 
 st.title("[beta]Startup Weekend Map for Japan")
